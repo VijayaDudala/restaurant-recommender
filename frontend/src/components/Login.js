fabcbaaf68bf90101547ff5cd632c1bba267ff41
vijju <vijjududala@gmail.com>
2026-05-18 13:05:31 +0530

    Fixed Netlify eslint error

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const navigate = useNavigate();
+  
 
   const handleLogin = () => {
     fetch("https://restaurant-recommender-api-laof.onrender.com/api/login/", {
